d6cf3a665f67f9074310e5c80718261e7f1cfb8b
Brian Clarke <15670716+clarkebd@users.noreply.github.com>
2018-08-08 20:41:51 -0400

    Initial checkin
    Chapter 7 assignment

---
+++ AssignSeat.java
@@ -0,0 +1,78 @@
+/**
+ * @author Brian
+ */
+
+package AirplaneReservations;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AssignSeat
+{
+
+    public void displayWelcome()
+    {
+        System.out.println(" - - - Airline Reservations - - -");
+    }
+
+    public int askClass()
+    {
+        System.out.print("\033[H\033[2J"); // clear the console
+        System.out.print("\nWhere would you like to sit:\n");
+        System.out.print("1 - First Class\n");
+        System.out.print("2 - Coach\n");
+        System.out.print("Choice? ");
+
+        Scanner input = new Scanner(System.in);
+        int area = input.nextInt();
+
+        return area;
+    }
+
+    
+    public int bookFirst()
+    {
+            boolean[] first = new boolean[5];
+            
+            // testing purposes only
+            first[0]=true; // seat full
+            first[1]=true; // seat full
+            //first[2]=true; // seat full
+            //first[3]=true; // seat full
+            //first[4]=true; // seat full
+            
+            
+            //check if space in first i.e. seats (0-4)
+            for (int loop=0;loop<5;loop++)
+            {
+                if (first[loop]==false)
+                {
+                    System.out.printf("Seat %d still available in First Class. Booking it for you!\n", loop);
+                    first[loop]=true;
+                    return loop;
+                } 
+            }
+        System.out.print(Arrays.toString(first));
+        return 0;
+    }
+    
+
+    public int bookCoach()
+    {
+            boolean[] coach = new boolean[5];
+            
+                 //check if space in coach i.e. seats (5-9)
+                for (int loop=0;loop<5;loop++)
+                {
+                    if (coach[loop]==false)
+                    {
+                        System.out.printf("Seat %d still available in Coach Class.Booking it for you!\n", loop);
+                        coach[loop]=true;
+                        return loop;
+                    } 
+                } 
+          System.out.print(Arrays.toString(coach));            
+          return 0;                
+    }
+
+}
